1021861eebf2226f873a7e0f227c14b69ff6a72a
Lucas <107668539+lfomar1@users.noreply.github.com>
2024-01-29 16:17:22 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Lucas Omar on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
